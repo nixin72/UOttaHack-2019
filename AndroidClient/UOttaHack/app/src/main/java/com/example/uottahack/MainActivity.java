f4d28ba5828dbd915c3519719e9a0e92d72152f4
phdumaresq <phdumaresq@gmail.com>
2019-02-10 07:50:31 -0500

    Working on getting sending post data

--- AndroidClient/UOttaHack/app/src/main/java/com/example/uottahack/MainActivity.java
+++ AndroidClient/UOttaHack/app/src/main/java/com/example/uottahack/MainActivity.java
@@ -5,16 +5,31 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
+import java.util.Scanner;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -22,6 +37,8 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import android.support.v4.app.ActivityCompat;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,16 +70,16 @@ public class MainActivity extends AppCompatActivity {
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if(checkPermission()) {
-
                     AudioSavePathInDevice =
                             Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
                                     CreateRandomAudioFileName(5) + "AudioRecording.3gp";
+                    Log.d("SOMETHING", "SOMETHING");
 
                     MediaRecorderReady();
 
                     try {
+                        mediaRecorder.setOutputFile(AudioSavePathInDevice);
                         mediaRecorder.prepare();
                         mediaRecorder.start();
                     } catch (IllegalStateException e) {
@@ -78,10 +95,10 @@ public class MainActivity extends AppCompatActivity {
 
                     Toast.makeText(MainActivity.this, "Recording started",
                             Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else {
                     requestPermission();
                 }
-
             }
         });
 
@@ -94,8 +111,43 @@ public class MainActivity extends AppCompatActivity {
                 buttonStart.setEnabled(true);
                 buttonStopPlayingRecording.setEnabled(false);
 
-                Toast.makeText(MainActivity.this, "Recording Completed",
-                        Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Recording Completed", Toast.LENGTH_LONG).show();
+
+                try {
+                    StringBuffer fileData = new StringBuffer();
+                    BufferedReader r = new BufferedReader(new FileReader(AudioSavePathInDevice));
+                    char[] buf = new char[3192926/4];
+                    int numRead = 0;
+                    while((numRead = r.read(buf)) != -1){
+                        String readData = String.valueOf(buf, 0, numRead);
+                        fileData.append(readData);
+                    }
+                    r.close();
+
+                    String content = fileData.toString();
+                    sendPost(content);
+
+//                    Process p = Runtime.getRuntime().exec(new String[]{"py", content});
+//                    InputStream pp = p.getInputStream();
+//
+//                    StringBuilder textBuilder = new StringBuilder();
+//                    try (Reader reader = new BufferedReader(new InputStreamReader
+//                            (pp, Charset.forName(StandardCharsets.UTF_8.name())))) {
+//                        int c = 0;
+//                        while ((c = reader.read()) != -1) {
+//                            textBuilder.append((char) c);
+//                        }
+//                    }
+//
+//                    Log.w("HERE", textBuilder.toString());
+//
+//
+//                    Log.w("HERE", "HERE");
+
+                }
+                catch (Exception e) {
+                    Log.e("FILE NOT FOUND", "");
+                }
             }
         });
 
@@ -103,16 +155,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) throws IllegalArgumentException,
                     SecurityException, IllegalStateException {
-
                 buttonStop.setEnabled(false);
                 buttonStart.setEnabled(false);
                 buttonStopPlayingRecording.setEnabled(true);
 
                 mediaPlayer = new MediaPlayer();
+
                 try {
                     mediaPlayer.setDataSource(AudioSavePathInDevice);
                     mediaPlayer.prepare();
                 } catch (IOException e) {
+                    Log.e("ERROR: ", e.getMessage());
                     e.printStackTrace();
                 }
 
@@ -133,11 +186,59 @@ public class MainActivity extends AppCompatActivity {
                 if(mediaPlayer != null){
                     mediaPlayer.stop();
                     mediaPlayer.release();
+
                     MediaRecorderReady();
                 }
             }
         });
+    }
 
+    private void sendPost(String data) throws Exception {
+        String url = "https://www.google.com";
+        Log.e("DATA: ", data);
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "");
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+
+        con.setDoOutput(true);
+        DataOutputStream wr;
+        try {
+            // TODO: DYING HERE
+            OutputStream out = con.getOutputStream();
+            wr = new DataOutputStream(out);
+//            wr.writeBytes(urlParameters);
+            wr.writeBytes(data);
+            wr.flush();
+            wr.close();
+        }
+        catch(Exception e) {
+            Log.e("ERROR", "DataOutputStream");
+        }
+
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        StringBuffer response = new StringBuffer();
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+        in.close();
+        Log.w("ERORR : ", response.toString());
     }
 
     public void MediaRecorderReady(){
